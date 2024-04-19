fixed scaling on pair data

LandSlideDyna/model/dataset.py
@@ -7,7 +7,6 @@ import torch
 from torch.utils.data import Dataset, DataLoader, Subset
 from sklearn.model_selection import train_test_split
 
-
 class DebrisStatePairsDataset(Dataset):
     def __init__(self, root_dir, array_size=128, apply_scaling=False):
         self.array_size = array_size
@@ -66,7 +65,7 @@ class DebrisStatePairsDataset(Dataset):
 
         # Compute min and max values over the subset
         for idx in subset.indices:
-            current_velocity_path, _, current_thickness_path, _ = self.data_files[idx]
+            current_velocity_path, next_velocity_path, current_thickness_path, next_thickness_path = self.data_files[idx]
 
             # Get the model directory from the current velocity path
             model_dir = os.path.dirname(os.path.dirname(os.path.dirname(current_velocity_path)))
@@ -77,12 +76,15 @@ class DebrisStatePairsDataset(Dataset):
             min_elevation = min(min_elevation, terrain.min())
             max_elevation = max(max_elevation, terrain.max())
 
-            velocity = np.load(current_velocity_path)
-            thickness = np.load(current_thickness_path)
-            min_velocity = min(min_velocity, velocity.min())
-            max_velocity = max(max_velocity, velocity.max())
-            min_thickness = min(min_thickness, thickness.min())
-            max_thickness = max(max_thickness, thickness.max())
+            current_velocity = np.load(current_velocity_path)
+            next_velocity = np.load(next_velocity_path)
+            current_thickness = np.load(current_thickness_path)
+            next_thickness = np.load(next_thickness_path)
+
+            min_velocity = min(min_velocity, current_velocity.min(), next_velocity.min())
+            max_velocity = max(max_velocity, current_velocity.max(), next_velocity.max())
+            min_thickness = min(min_thickness, current_thickness.min(), next_thickness.min())
+            max_thickness = max(max_thickness, current_thickness.max(), next_thickness.max())
 
         return min_elevation, max_elevation, min_velocity, max_velocity, min_thickness, max_thickness
 
@@ -92,9 +94,8 @@ class DebrisStatePairsDataset(Dataset):
 
         min_elevation, max_elevation, min_velocity, max_velocity, min_thickness, max_thickness = self.scaling_factors
 
-        # Apply Min-Max scaling to each feature
         terrain_scaled = (terrain - min_elevation) / (max_elevation - min_elevation) * 10
-        velocity_scaled = (velocity - min_velocity) / (max_velocity - min_velocity) * 10 
+        velocity_scaled = (velocity - min_velocity) / (max_velocity - min_velocity) * 10
         thickness_scaled = (thickness - min_thickness) / (max_thickness - min_thickness) * 10
 
         return terrain_scaled, velocity_scaled, thickness_scaled
@@ -119,7 +120,6 @@ class DebrisStatePairsDataset(Dataset):
         terrain = np.load(terrain_path)
 
         if self.apply_scaling:
-            # Apply scaling to the loaded data
             terrain_scaled, current_velocity_scaled, current_thickness_scaled = self.scale_data(
                 terrain, current_velocity, current_thickness
             )
@@ -127,9 +127,11 @@ class DebrisStatePairsDataset(Dataset):
                 terrain, next_velocity, next_thickness
             )
         else:
-            # No scaling applied, use the original values
-            terrain_scaled, current_velocity_scaled, current_thickness_scaled = terrain, current_velocity, current_thickness
-            next_velocity_scaled, next_thickness_scaled = next_velocity, next_thickness
+            terrain_scaled = terrain
+            current_velocity_scaled = current_velocity
+            current_thickness_scaled = current_thickness
+            next_velocity_scaled = next_velocity
+            next_thickness_scaled = next_thickness
 
         # Stack arrays as channels for CNN input and output
         cnn_input = np.stack((terrain_scaled, current_velocity_scaled, current_thickness_scaled), axis=0)
@@ -178,6 +180,178 @@ class DebrisStatePairsDataset(Dataset):
         # Return the DataLoaders
         return train_loader, val_loader, test_loader
 
+# class DebrisStatePairsDataset(Dataset):
+#     def __init__(self, root_dir, array_size=128, apply_scaling=False):
+#         self.array_size = array_size
+#         self.data_files = []
+#         self.model_ids = []  # List to store the model IDs
+#         self.terrain_files = {}
+#         self.apply_scaling = apply_scaling
+#         self.scaling_factors = None
+
+#         def get_state_number(file_path):
+#             return int(file_path.split('_')[-1].split('.')[0])
+
+#         model_dirs = glob.glob(os.path.join(root_dir, '*'))
+        
+#         for model_dir in model_dirs:
+#             if os.path.isdir(model_dir):
+#                 # Extract the model ID from the model_dir path
+#                 model_id = os.path.basename(model_dir)
+
+#                 file_patterns = {
+#                     'velocity': os.path.join(model_dir, f'04_FinalProcessedData_{str(self.array_size)}', 'velocity', '*_velocity_*.npy'),
+#                     'thickness': os.path.join(model_dir, f'04_FinalProcessedData_{str(self.array_size)}', 'thickness', '*_thickness_*.npy')
+#                 }
+#                 file_collections = {key: sorted(glob.glob(pattern), key=get_state_number) for key, pattern in file_patterns.items()}
+
+#                 terrain_pattern = os.path.join(model_dir, f'04_FinalProcessedData_{str(self.array_size)}', 'elevation', '*_elevation.npy')
+#                 terrain_file = glob.glob(terrain_pattern)
+
+#                 if terrain_file:
+#                     self.terrain_files[model_dir] = terrain_file[0]
+
+#                 for i in range(len(file_collections['velocity']) - 1):
+#                     current_state_velocity = get_state_number(file_collections['velocity'][i])
+#                     next_state_velocity = get_state_number(file_collections['velocity'][i + 1])
+#                     current_state_thickness = get_state_number(file_collections['thickness'][i])
+#                     next_state_thickness = get_state_number(file_collections['thickness'][i + 1])
+
+#                     if (next_state_velocity == current_state_velocity + 1) and (next_state_thickness == current_state_thickness + 1):
+#                         self.data_files.append((file_collections['velocity'][i], file_collections['velocity'][i + 1],
+#                                                 file_collections['thickness'][i], file_collections['thickness'][i + 1]))
+#                         # Append the model_id for each valid data point
+#                         self.model_ids.append(model_id)
+
+#     def compute_scaling_factors(self, subset):
+#         # Ensure that scaling is intended before computing factors
+#         if not self.apply_scaling:
+#             raise RuntimeError("Scaling factors called to be computed when scaling is not applied.")
+
+#         # Initialize min and max values with infinities
+#         min_elevation = np.inf
+#         max_elevation = -np.inf
+#         min_velocity = np.inf
+#         max_velocity = -np.inf
+#         min_thickness = np.inf
+#         max_thickness = -np.inf
+
+#         # Compute min and max values over the subset
+#         for idx in subset.indices:
+#             current_velocity_path, _, current_thickness_path, _ = self.data_files[idx]
+
+#             # Get the model directory from the current velocity path
+#             model_dir = os.path.dirname(os.path.dirname(os.path.dirname(current_velocity_path)))
+
+#             # Load the corresponding terrain file
+#             terrain_path = self.terrain_files[model_dir]
+#             terrain = np.load(terrain_path)
+#             min_elevation = min(min_elevation, terrain.min())
+#             max_elevation = max(max_elevation, terrain.max())
+
+#             velocity = np.load(current_velocity_path)
+#             thickness = np.load(current_thickness_path)
+#             min_velocity = min(min_velocity, velocity.min())
+#             max_velocity = max(max_velocity, velocity.max())
+#             min_thickness = min(min_thickness, thickness.min())
+#             max_thickness = max(max_thickness, thickness.max())
+
+#         return min_elevation, max_elevation, min_velocity, max_velocity, min_thickness, max_thickness
+
+#     def scale_data(self, terrain, velocity, thickness):
+#         if self.scaling_factors is None:
+#             raise RuntimeError("Scaling factors not computed.")
+
+#         min_elevation, max_elevation, min_velocity, max_velocity, min_thickness, max_thickness = self.scaling_factors
+
+#         # Apply Min-Max scaling to each feature
+#         terrain_scaled = (terrain - min_elevation) / (max_elevation - min_elevation) * 10
+#         velocity_scaled = (velocity - min_velocity) / (max_velocity - min_velocity) * 10 
+#         thickness_scaled = (thickness - min_thickness) / (max_thickness - min_thickness) * 10
+
+#         return terrain_scaled, velocity_scaled, thickness_scaled
+
+#     def __len__(self):
+#         return len(self.data_files)
+
+#     def __getitem__(self, idx):
+#         current_velocity_path, next_velocity_path, current_thickness_path, next_thickness_path = self.data_files[idx]
+        
+#         # Load current and next states for velocity and thickness
+#         current_velocity = np.load(current_velocity_path)
+#         next_velocity = np.load(next_velocity_path)
+#         current_thickness = np.load(current_thickness_path)
+#         next_thickness = np.load(next_thickness_path)
+
+#         # Get the model directory from the current velocity path
+#         model_dir = os.path.dirname(os.path.dirname(os.path.dirname(current_velocity_path)))
+
+#         # Load the corresponding terrain file
+#         terrain_path = self.terrain_files[model_dir]
+#         terrain = np.load(terrain_path)
+
+#         if self.apply_scaling:
+#             # Apply scaling to the loaded data
+#             terrain_scaled, current_velocity_scaled, current_thickness_scaled = self.scale_data(
+#                 terrain, current_velocity, current_thickness
+#             )
+#             _, next_velocity_scaled, next_thickness_scaled = self.scale_data(
+#                 terrain, next_velocity, next_thickness
+#             )
+#         else:
+#             # No scaling applied, use the original values
+#             terrain_scaled, current_velocity_scaled, current_thickness_scaled = terrain, current_velocity, current_thickness
+#             next_velocity_scaled, next_thickness_scaled = next_velocity, next_thickness
+
+#         # Stack arrays as channels for CNN input and output
+#         cnn_input = np.stack((terrain_scaled, current_velocity_scaled, current_thickness_scaled), axis=0)
+#         cnn_output = np.stack((next_velocity_scaled, next_thickness_scaled), axis=0)
+
+#         return torch.from_numpy(cnn_input).float(), torch.from_numpy(cnn_output).float()
+
+#     def create_dataloaders(self, split_proportions, batch_size, random_state=42):
+#         # Unpack the proportions for clarity
+#         train_proportion, val_proportion, test_proportion = split_proportions
+        
+#         # Assert that the proportions sum to 1
+#         assert np.isclose(sum(split_proportions), 1.0), "Proportions must sum up to 1."
+        
+#         # Create a list of unique model IDs
+#         unique_model_ids = np.unique(self.model_ids)
+        
+#         # Split model IDs into train, val, and test sets
+#         train_model_ids, temp_model_ids = train_test_split(
+#             unique_model_ids, test_size=(val_proportion + test_proportion), random_state=random_state
+#         )
+#         val_model_ids, test_model_ids = train_test_split(
+#             temp_model_ids, test_size=test_proportion / (val_proportion + test_proportion), random_state=random_state
+#         )
+        
+#         # Now, filter the dataset's data points based on the model IDs
+#         train_indices = [i for i, model_id in enumerate(self.model_ids) if model_id in train_model_ids]
+#         val_indices = [i for i, model_id in enumerate(self.model_ids) if model_id in val_model_ids]
+#         test_indices = [i for i, model_id in enumerate(self.model_ids) if model_id in test_model_ids]
+        
+#         # Compute scaling factors based on the train subset, but only if scaling is applied
+#         if self.apply_scaling:
+#             train_subset = Subset(self, train_indices)
+#             self.scaling_factors = self.compute_scaling_factors(train_subset)
+
+#         # Create subsets
+#         train_dataset = Subset(self, train_indices)
+#         val_dataset = Subset(self, val_indices)
+#         test_dataset = Subset(self, test_indices)
+        
+#         # DataLoaders
+#         train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+#         val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+#         test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+        
+#         # Return the DataLoaders
+#         return train_loader, val_loader, test_loader
+    
+
+##NOT SURE WHAT THIS IS ###
 # class DebrisStatePairsDataset(Dataset):
 #     def __init__(self, root_dir, array_size=128, apply_scaling=False):
 #         self.array_size = array_size
@@ -339,7 +513,7 @@ class DebrisStatePairsDataset(Dataset):
 
 
 class DebrisStateSeriesDataset(Dataset):
-    def __init__(self, root_dir, array_size, apply_scaling=False, sequence_length=3):
+    def __init__(self, root_dir, array_size, sequence_length, apply_scaling=False):
         self.array_size = array_size
         self.data_files = []
         self.model_ids = []  # List to store the model IDs

LandSlideDyna/model/train.py
@@ -62,6 +62,8 @@ class TrainerPairs:
         # After training, plot the training and validation losses
         self.plot_losses()
 
+        self.scaling_factors = train_loader.dataset.dataset.scaling_factors
+
     def validate(self, val_loader):
         self.model.eval()
         total_val_loss = 0
@@ -119,6 +121,74 @@ class TrainerPairs:
                 total_loss += loss.item()
         avg_loss = total_loss / len(test_loader)
         print(f'Test Loss: {avg_loss:.4f}')
+
+
+    def create_inference_input(self, root_dir, model_number, state_number, array_size):
+        # Construct the file paths based on the provided parameters
+        model_dir = os.path.join(root_dir, str(model_number))
+        elevation_file = os.path.join(model_dir, f'04_FinalProcessedData_{array_size}', 'elevation', f'{model_number}_elevation.npy')
+        velocity_file = os.path.join(model_dir, f'04_FinalProcessedData_{array_size}', 'velocity', f'{model_number}_velocity_{state_number}.npy')
+        thickness_file = os.path.join(model_dir, f'04_FinalProcessedData_{array_size}', 'thickness', f'{model_number}_thickness_{state_number}.npy')
+
+        # Load the data arrays
+        elevation = np.load(elevation_file)
+        velocity = np.load(velocity_file)
+        thickness = np.load(thickness_file)
+
+        # Scale the data arrays
+        min_elevation, max_elevation, min_velocity, max_velocity, min_thickness, max_thickness = self.scaling_factors
+        elevation_scaled = (elevation - min_elevation) / (max_elevation - min_elevation) * 10
+        velocity_scaled = (velocity - min_velocity) / (max_velocity - min_velocity) * 10
+        thickness_scaled = (thickness - min_thickness) / (max_thickness - min_thickness) * 10
+
+        # Stack the scaled data arrays to create the input tensor
+        input_data = np.stack((elevation_scaled, thickness_scaled, velocity_scaled), axis=0)
+
+        # Convert to PyTorch tensor and move to the specified device
+        input_tensor = torch.from_numpy(input_data).float().to(self.device)
+
+        return input_tensor
+
+    def infer(self, initial_input, num_timesteps):
+        self.model.eval()
+        device = self.device
+        scaling_factors = self.scaling_factors
+        
+        # Ensure initial_input is a PyTorch tensor
+        if not isinstance(initial_input, torch.Tensor):
+            initial_input = torch.tensor(initial_input, dtype=torch.float32)
+
+        # Move the initial input to the same device as the model
+        initial_input = initial_input.to(device)
+
+        # Extract the elevation channel from the initial input
+        elevation = initial_input[0].unsqueeze(0)
+
+        # Initialize the input tensor with the initial input
+        input_tensor = initial_input.unsqueeze(0)  # Add batch dimension
+
+        # Initialize a dictionary to store the inferred states
+        inferred_states = {}
+
+        with torch.no_grad():
+            for t in range(num_timesteps):
+                # Perform inference
+                next_state = self.model(input_tensor)
+
+                # Scale and store the output
+                min_elevation, max_elevation, min_velocity, max_velocity, min_thickness, max_thickness = scaling_factors
+                next_state_unscaled = next_state.clone()
+                next_state_unscaled[:, 0, ...] = next_state[:, 0, ...] * (max_thickness - min_thickness) / 10 + min_thickness
+                next_state_unscaled[:, 1, ...] = next_state[:, 1, ...] * (max_velocity - min_velocity) / 10 + min_velocity
+                inferred_states[t + 1] = next_state_unscaled.squeeze(0).cpu().numpy()
+
+                # Stack the elevation channel with the inferred next state
+                next_state_with_elevation = torch.cat((elevation, next_state.squeeze(0)), dim=0)
+
+                # Update the input tensor for the next iteration
+                input_tensor = next_state_with_elevation.unsqueeze(0)  # Add batch dimension
+
+        return inferred_states
 
     def plot_predictions(self, loader, num_predictions=5):
         self.model.eval()
@@ -450,122 +520,77 @@ class TrainerSeries:
 
         return inferred_states
 
-    # def infer(self, initial_sequence, num_timesteps):
+
+
+    # def plot_losses(self):
+    #     plt.figure(figsize=(10, 5))
+    #     plt.plot(self.training_losses, label='Training Loss')
+    #     plt.plot(self.validation_losses, label='Validation Loss')
+    #     plt.title('Training and Validation Losses')
+    #     plt.xlabel('Epochs')
+    #     plt.ylabel('Loss')
+    #     plt.legend()
+    #     plt.grid(True)
+    #     plt.tight_layout()
+
+    #     # Save the plot as an image file
+    #     plot_file = 'loss_plot.png'
+    #     plot_path = os.path.join(self.checkpoint_dir, plot_file)
+    #     plt.savefig(plot_path)
+    #     print(f'Loss plot saved to {plot_path}')
+
+    #     # Show the plot
+    #     plt.show()
+
+    # def plot_predictions(self, test_loader, num_predictions=5):
     #     self.model.eval()
-    #     device = next(self.model.parameters()).device
-    #     scaling_factors = self.scaling_factors
-        
-    #     # Ensure initial sequence is a PyTorch tensor
-    #     if not isinstance(initial_sequence, torch.Tensor):
-    #         initial_sequence = torch.tensor(initial_sequence, dtype=torch.float32)
-
-    #     # Add batch dimension if necessary
-    #     if initial_sequence.dim() == 4:
-    #         initial_sequence = initial_sequence.unsqueeze(0)
-
-    #     # Move the initial sequence to the same device as the model
-    #     initial_sequence = initial_sequence.to(device)
-
-    #     # Initialize the inferred sequence with the initial sequence
-    #     inferred_sequence = [initial_sequence]
-
-    #     # Initialize a dictionary to store the inferred states
-    #     inferred_states = {}
-
     #     with torch.no_grad():
-    #         for t in range(num_timesteps):
-    #             # Get the last sequence_length states from the inferred sequence
-    #             input_sequence = torch.cat(inferred_sequence[-self.sequence_length:], dim=1)
+    #         for i, (sequence, next_state) in enumerate(test_loader):
+    #             if i >= num_predictions:
+    #                 break
 
-    #             # Perform inference
-    #             next_state, _ = self.model(input_sequence)
+    #             sequence = sequence.to(self.device)
+    #             next_state = next_state.to(self.device)
 
-    #             # Unscale the inferred next state
-    #             min_elevation, max_elevation, min_velocity, max_velocity, min_thickness, max_thickness = scaling_factors
-    #             next_state_unscaled = next_state.clone()
-    #             next_state_unscaled[0, 0, ...] = next_state[0, 0, ...] * (max_thickness - min_thickness) / 10 + min_thickness
-    #             next_state_unscaled[0, 1, ...] = next_state[0, 1, ...] * (max_velocity - min_velocity) / 10 + min_velocity
+    #             predictions, _ = self.model(sequence)
 
-    #             # Append the unscaled inferred next state to the sequence
-    #             inferred_sequence.append(next_state_unscaled.unsqueeze(1))
+    #             # Move the data back to the CPU for plotting
+    #             next_state = next_state.cpu().numpy()
+    #             predictions = predictions.cpu().numpy()
 
-    #             # Store the unscaled inferred state in the dictionary
-    #             inferred_states[t + 1] = next_state_unscaled.cpu().numpy()
+    #             # Plot the expected and predicted results side by side
+    #             fig, axes = plt.subplots(3, 2, figsize=(12, 18))
 
-    #     # Concatenate the inferred sequence along the time dimension
-    #     inferred_sequence = torch.cat(inferred_sequence, dim=1)
+    #             # Process and plot each type of data (thickness, velocity)
+    #             for index, title in enumerate(['Thickness', 'Velocity']):
+    #                 # Determine color limits based on the expected values
+    #                 vmin = np.min(next_state[0, index])
+    #                 vmax = np.max(next_state[0, index])
 
-    #     return inferred_sequence, inferred_states
+    #                 # Expected
+    #                 im = axes[index, 0].imshow(next_state[0, index], cmap='viridis', vmin=vmin, vmax=vmax)
+    #                 axes[index, 0].set_title(f"Expected {title}")
+    #                 axes[index, 0].axis('off')
+    #                 fig.colorbar(im, ax=axes[index, 0], orientation='vertical')
 
-    def plot_losses(self):
-        plt.figure(figsize=(10, 5))
-        plt.plot(self.training_losses, label='Training Loss')
-        plt.plot(self.validation_losses, label='Validation Loss')
-        plt.title('Training and Validation Losses')
-        plt.xlabel('Epochs')
-        plt.ylabel('Loss')
-        plt.legend()
-        plt.grid(True)
-        plt.tight_layout()
+    #                 # Predicted
+    #                 im = axes[index, 1].imshow(predictions[0, index], cmap='viridis', vmin=vmin, vmax=vmax)
+    #                 axes[index, 1].set_title(f"Predicted {title}")
+    #                 axes[index, 1].axis('off')
+    #                 fig.colorbar(im, ax=axes[index, 1], orientation='vertical')
 
-        # Save the plot as an image file
-        plot_file = 'loss_plot.png'
-        plot_path = os.path.join(self.checkpoint_dir, plot_file)
-        plt.savefig(plot_path)
-        print(f'Loss plot saved to {plot_path}')
+    #             # Compute and plot the differences for thickness and velocity
+    #             for index, title in enumerate(['Thickness', 'Velocity']):
+    #                 diff = np.abs(next_state[0, index] - predictions[0, index])
+    #                 vmax = np.max(diff)  # Dynamic range based on the actual differences observed
 
-        # Show the plot
-        plt.show()
+    #                 im = axes[2, index].imshow(diff, cmap='hot', vmin=0, vmax=vmax)
+    #                 axes[2, index].set_title(f"Difference in {title}")
+    #                 axes[2, index].axis('off')
+    #                 fig.colorbar(im, ax=axes[2, index], orientation='vertical')
 
-    def plot_predictions(self, test_loader, num_predictions=5):
-        self.model.eval()
-        with torch.no_grad():
-            for i, (sequence, next_state) in enumerate(test_loader):
-                if i >= num_predictions:
-                    break
-
-                sequence = sequence.to(self.device)
-                next_state = next_state.to(self.device)
-
-                predictions, _ = self.model(sequence)
-
-                # Move the data back to the CPU for plotting
-                next_state = next_state.cpu().numpy()
-                predictions = predictions.cpu().numpy()
-
-                # Plot the expected and predicted results side by side
-                fig, axes = plt.subplots(3, 2, figsize=(12, 18))
-
-                # Process and plot each type of data (thickness, velocity)
-                for index, title in enumerate(['Thickness', 'Velocity']):
-                    # Determine color limits based on the expected values
-                    vmin = np.min(next_state[0, index])
-                    vmax = np.max(next_state[0, index])
-
-                    # Expected
-                    im = axes[index, 0].imshow(next_state[0, index], cmap='viridis', vmin=vmin, vmax=vmax)
-                    axes[index, 0].set_title(f"Expected {title}")
-                    axes[index, 0].axis('off')
-                    fig.colorbar(im, ax=axes[index, 0], orientation='vertical')
-
-                    # Predicted
-                    im = axes[index, 1].imshow(predictions[0, index], cmap='viridis', vmin=vmin, vmax=vmax)
-                    axes[index, 1].set_title(f"Predicted {title}")
-                    axes[index, 1].axis('off')
-                    fig.colorbar(im, ax=axes[index, 1], orientation='vertical')
-
-                # Compute and plot the differences for thickness and velocity
-                for index, title in enumerate(['Thickness', 'Velocity']):
-                    diff = np.abs(next_state[0, index] - predictions[0, index])
-                    vmax = np.max(diff)  # Dynamic range based on the actual differences observed
-
-                    im = axes[2, index].imshow(diff, cmap='hot', vmin=0, vmax=vmax)
-                    axes[2, index].set_title(f"Difference in {title}")
-                    axes[2, index].axis('off')
-                    fig.colorbar(im, ax=axes[2, index], orientation='vertical')
-
-                plt.tight_layout()
-                plt.show()
+    #             plt.tight_layout()
+    #             plt.show()
 
 class CustomDebrisLoss(nn.Module):
     def __init__(self, loss_fn_zero=nn.MSELoss(), loss_fn_debris=nn.L1Loss(), debris_weight=0.75):