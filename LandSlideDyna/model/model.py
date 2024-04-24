fixed scaling dropout regularisation

LandSlideDyna/model/model.py
@@ -6,45 +6,111 @@ import torch.nn.functional as F
 
 ### STANDALONE CNNs ###
 
+# Added dropout2D
 class CNN(nn.Module):
-    def __init__(self):
+    def __init__(self, dropout_rate=0.25):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)  # Input: 3 channels, Output: 32 channels
         self.bn1 = nn.BatchNorm2d(32)
+        self.dropout1 = nn.Dropout2d(p=dropout_rate)  # Dropout layer after the first batch norm
+        
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(64)
+        self.dropout2 = nn.Dropout2d(p=dropout_rate)  # Dropout layer after the second batch norm
+
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.bn3 = nn.BatchNorm2d(128)
+        self.dropout3 = nn.Dropout2d(p=dropout_rate)  # Dropout layer after the third batch norm
+
         self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
         self.bn4 = nn.BatchNorm2d(256)
+        self.dropout4 = nn.Dropout2d(p=dropout_rate)  # Dropout layer after the fourth batch norm
+
         self.pool = nn.MaxPool2d(2, 2)
         self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
         self.conv5 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
         self.bn5 = nn.BatchNorm2d(128)
+        self.dropout5 = nn.Dropout2d(p=dropout_rate)  # Dropout layer after the fifth batch norm
+
         self.conv6 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
         self.bn6 = nn.BatchNorm2d(64)
+        self.dropout6 = nn.Dropout2d(p=dropout_rate)  # Dropout layer after the sixth batch norm
+
         self.conv7 = nn.Conv2d(64, 2, kernel_size=3, padding=1)  # Output: 2 channels (velocity and thickness)
 
     def forward(self, x):
         # Encoder path
-        x1 = F.relu(self.bn1(self.conv1(x)))     # 64x64x32
-        x2 = self.pool(x1)                       # 32x32x32
-        x3 = F.relu(self.bn2(self.conv2(x2)))    # 32x32x64
-        x4 = self.pool(x3)                       # 16x16x64
-        x5 = F.relu(self.bn3(self.conv3(x4)))    # 16x16x128
-        x6 = self.pool(x5)                       # 8x8x128
-        x7 = F.relu(self.bn4(self.conv4(x6)))    # 8x8x256
+        x1 = F.relu(self.bn1(self.conv1(x)))  # 64x64x32
+        x1 = self.dropout1(x1)
+        x2 = self.pool(x1)                    # 32x32x32
+
+        x3 = F.relu(self.bn2(self.conv2(x2)))  # 32x32x64
+        x3 = self.dropout2(x3)
+        x4 = self.pool(x3)                     # 16x16x64
+
+        x5 = F.relu(self.bn3(self.conv3(x4)))  # 16x16x128
+        x5 = self.dropout3(x5)
+        x6 = self.pool(x5)                     # 8x8x128
+
+        x7 = F.relu(self.bn4(self.conv4(x6)))  # 8x8x256
+        x7 = self.dropout4(x7)
 
         # Decoder path
-        x8 = self.up(x7)                         # 16x16x256
-        x9 = F.relu(self.bn5(self.conv5(x8)))    # 16x16x128
-        x10 = self.up(x9)                        # 32x32x128
+        x8 = self.up(x7)                       # 16x16x256
+        x9 = F.relu(self.bn5(self.conv5(x8)))  # 16x16x128
+        x9 = self.dropout5(x9)
+
+        x10 = self.up(x9)                      # 32x32x128
         x11 = F.relu(self.bn6(self.conv6(x10)))  # 32x32x64
-        x12 = self.up(x11)                       # 64x64x64
+        x11 = self.dropout6(x11)
+
+        x12 = self.up(x11)                     # 64x64x64
 
         # Output layer
-        x13 = self.conv7(x12)                    # 64x64x2
+        x13 = self.conv7(x12)                  # 64x64x2
         return x13
+
+
+# class CNN(nn.Module):
+#     def __init__(self):
+#         super(CNN, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)  # Input: 3 channels, Output: 32 channels
+#         self.bn1 = nn.BatchNorm2d(32)
+#         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+#         self.bn2 = nn.BatchNorm2d(64)
+#         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+#         self.bn3 = nn.BatchNorm2d(128)
+#         self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+#         self.bn4 = nn.BatchNorm2d(256)
+#         self.pool = nn.MaxPool2d(2, 2)
+#         self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+#         self.conv5 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
+#         self.bn5 = nn.BatchNorm2d(128)
+#         self.conv6 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
+#         self.bn6 = nn.BatchNorm2d(64)
+#         self.conv7 = nn.Conv2d(64, 2, kernel_size=3, padding=1)  # Output: 2 channels (velocity and thickness)
+
+#     def forward(self, x):
+#         # Encoder path
+#         x1 = F.relu(self.bn1(self.conv1(x)))     # 64x64x32
+#         x2 = self.pool(x1)                       # 32x32x32
+#         x3 = F.relu(self.bn2(self.conv2(x2)))    # 32x32x64
+#         x4 = self.pool(x3)                       # 16x16x64
+#         x5 = F.relu(self.bn3(self.conv3(x4)))    # 16x16x128
+#         x6 = self.pool(x5)                       # 8x8x128
+#         x7 = F.relu(self.bn4(self.conv4(x6)))    # 8x8x256
+
+#         # Decoder path
+#         x8 = self.up(x7)                         # 16x16x256
+#         x9 = F.relu(self.bn5(self.conv5(x8)))    # 16x16x128
+#         x10 = self.up(x9)                        # 32x32x128
+#         x11 = F.relu(self.bn6(self.conv6(x10)))  # 32x32x64
+#         x12 = self.up(x11)                       # 64x64x64
+
+#         # Output layer
+#         x13 = self.conv7(x12)                    # 64x64x2
+#         return x13
     
 class SimpleUNet(nn.Module):
     def __init__(self):
@@ -136,9 +202,11 @@ class LargeUNet(nn.Module):
 
         # Final convolution
         return self.final_conv(dec0)
-    
+
+# added dropout2D
+
 class UNet(nn.Module):
-    def __init__(self, in_channels, out_channels, features=[64, 128, 256, 512, 1024]):
+    def __init__(self, in_channels, out_channels, features=[64, 128, 256, 512, 1024], dropout_rate=0.25):
         super(UNet, self).__init__()
         self.encoders = nn.ModuleList()
         self.decoders = nn.ModuleList()
@@ -147,12 +215,12 @@ class UNet(nn.Module):
         # Encoder path
         for feature in features:
             self.encoders.append(
-                UNet._block(in_channels, feature)
+                UNet._block(in_channels, feature, dropout_rate)
             )
             in_channels = feature
         
         # Bottleneck
-        self.bottleneck = UNet._block(features[-1], features[-1] * 2)
+        self.bottleneck = UNet._block(features[-1], features[-1] * 2, dropout_rate)
 
         # Decoder path
         for feature in reversed(features):
@@ -160,18 +228,19 @@ class UNet(nn.Module):
                 nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2)
             )
             self.decoders.append(
-                UNet._block(feature * 2, feature)
+                UNet._block(feature * 2, feature, dropout_rate)
             )
         
         # Final convolution
         self.final_layer = nn.Conv2d(features[0], out_channels, kernel_size=1)
 
     @staticmethod
-    def _block(in_channels, features):
+    def _block(in_channels, features, dropout_rate=0.5):
         return nn.Sequential(
             nn.Conv2d(in_channels, features, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(features),
             nn.ReLU(inplace=True),
+            nn.Dropout2d(dropout_rate),  # Using Dropout2d
             nn.Conv2d(features, features, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(features),
             nn.ReLU(inplace=True)
@@ -197,7 +266,7 @@ class UNet(nn.Module):
             x = self.decoders[idx](x)
             skip_connection = skip_connections[idx // 2]
 
-            # If the input sizes are different, resize the skip connection to match
+            # Resize skip connection if necessary
             if x.shape != skip_connection.shape:
                 x = F.interpolate(x, size=skip_connection.shape[2:])
 
@@ -205,6 +274,80 @@ class UNet(nn.Module):
             x = self.decoders[idx + 1](x)
 
         return self.final_layer(x)
+
+
+# class UNet(nn.Module):
+#     def __init__(self, in_channels, out_channels, features=[64, 128, 256, 512, 1024], dropout_rate=0.5):
+#         super(UNet, self).__init__()
+#         self.encoders = nn.ModuleList()
+#         self.decoders = nn.ModuleList()
+#         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+#         # Encoder path
+#         for feature in features:
+#             self.encoders.append(
+#                 UNet._block(in_channels, feature, dropout_rate)
+#             )
+#             in_channels = feature
+        
+#         # Bottleneck
+#         self.bottleneck = UNet._block(features[-1], features[-1] * 2, dropout_rate)
+
+#         # Decoder path
+#         for feature in reversed(features):
+#             self.decoders.append(
+#                 nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2)
+#             )
+#             self.decoders.append(
+#                 UNet._block(feature * 2, feature, dropout_rate)
+#             )
+        
+#         # Final convolution
+#         self.final_layer = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+#     @staticmethod
+#     def _block(in_channels, features, dropout_rate=0.5):
+#         return nn.Sequential(
+#             nn.Conv2d(in_channels, features, kernel_size=3, padding=1, bias=False),
+#             nn.BatchNorm2d(features),
+#             nn.ReLU(inplace=True),
+#             nn.Dropout(dropout_rate),  # Add dropout after activation
+#             nn.Conv2d(features, features, kernel_size=3, padding=1, bias=False),
+#             nn.BatchNorm2d(features),
+#             nn.ReLU(inplace=True)
+#         )
+
+#     def forward(self, x):
+#         skip_connections = []
+
+#         # Encoder
+#         for encoder in self.encoders:
+#             x = encoder(x)
+#             skip_connections.append(x)
+#             x = self.pool(x)
+
+#         # Bottleneck
+#         x = self.bottleneck(x)
+
+#         # Reverse the skip connections
+#         skip_connections = skip_connections[::-1]
+
+#         # Decoder
+#         for idx in range(0, len(self.decoders), 2):
+#             x = self.decoders[idx](x)
+#             skip_connection = skip_connections[idx // 2]
+
+#             # If the input sizes are different, resize the skip connection to match
+#             if x.shape != skip_connection.shape:
+#                 x = F.interpolate(x, size=skip_connection.shape[2:])
+
+#             x = torch.cat((skip_connection, x), dim=1)
+#             x = self.decoders[idx + 1](x)
+
+#         return self.final_layer(x)
+
+
+
     
 ### CNN-RNN HYBRIDS ###
 
@@ -396,52 +539,6 @@ class DecoderBlock(nn.Module):
         x = self.conv_block(x)
         return x
 
-# class UNetLSTM(nn.Module):
-#     def __init__(self, input_channels=3, output_channels=2, hidden_size=512):
-#         super().__init__()
-#         self.enc1 = EncoderBlock(input_channels, 16)
-#         self.enc2 = EncoderBlock(16, 32)
-#         self.enc3 = EncoderBlock(32, 64)
-#         self.enc4 = EncoderBlock(64, 128)
-#         self.enc5 = EncoderBlock(128, 256)
-#         self.conv_block = ConvBlock(256, 512)
-#         self.gap = nn.AdaptiveAvgPool2d((1, 1))
-#         self.lstm = nn.LSTM(512, hidden_size, batch_first=True)
-#         self.fc = nn.Linear(hidden_size, 512)
-#         self.reshape = nn.Unflatten(1, (512, 1, 1))
-#         self.dec5 = DecoderBlock(512, 256)
-#         self.dec4 = DecoderBlock(256, 128)
-#         self.dec3 = DecoderBlock(128, 64)
-#         self.dec2 = DecoderBlock(64, 32)
-#         self.dec1 = DecoderBlock(32, 16)
-#         self.final_conv = nn.Conv2d(16, output_channels, kernel_size=1)
-
-#     def forward(self, x):
-#         batch_size, seq_len, _, _, _ = x.size()
-#         outputs = []
-#         for t in range(seq_len):
-#             x_t, skip1 = self.enc1(x[:, t])
-#             x_t, skip2 = self.enc2(x_t)
-#             x_t, skip3 = self.enc3(x_t)
-#             x_t, skip4 = self.enc4(x_t)
-#             x_t, skip5 = self.enc5(x_t)
-#             x_t = self.conv_block(x_t)
-#             x_t = self.gap(x_t)
-#             x_t = x_t.view(batch_size, -1).unsqueeze(1)
-#             outputs.append(x_t)
-#         x = torch.cat(outputs, dim=1)
-
-#         x, _ = self.lstm(x)
-#         x = self.fc(x[:, -1, :])
-#         x = self.reshape(x)
-
-#         x = self.dec5(x, skip5)
-#         x = self.dec4(x, skip4)
-#         x = self.dec3(x, skip3)
-#         x = self.dec2(x, skip2)
-#         x = self.dec1(x, skip1)
-#         x = self.final_conv(x)
-#         return x, None
 
 class UNetLSTM(nn.Module):
     def __init__(self, input_channels=3, output_channels=2, hidden_size=512):

LandSlideDyna/model/train.py
@@ -86,9 +86,11 @@ class TrainerPairs:
         torch.save(self.model.state_dict(), checkpoint_path)
         print(f'Model saved to {checkpoint_path}')
 
-    def load_checkpoint(self, checkpoint_path):
+    def load_checkpoint(self, checkpoint_path, train_loader):
         checkpoint = torch.load(checkpoint_path)
         self.model.load_state_dict(checkpoint)
+        self.scaling_factors = train_loader.dataset.dataset.scaling_factors
+        
         print(f'Model loaded from {checkpoint_path}')
 
     def save_losses(self, epoch):
@@ -115,40 +117,6 @@ class TrainerPairs:
         self.validation_losses = losses['validation_losses']
         print(f'Losses loaded from {losses_path}')
 
-
-    # def test(self, test_loader):
-    #     self.model.eval()
-    #     mse_criterion = nn.MSELoss()
-    #     total_l1_loss = 0
-    #     total_mse = 0
-    #     total_psnr = 0
-        
-    #     with torch.no_grad():
-    #         for current, next_state in test_loader:
-    #             current = current.to(self.device)
-    #             next_state = next_state.to(self.device)
-                
-    #             predictions = self.model(current)
-    #             l1_loss = self.criterion(predictions, next_state)
-    #             mse_loss = mse_criterion(predictions, next_state)
-                
-    #             total_l1_loss += l1_loss.item()
-    #             total_mse += mse_loss.item()
-                
-    #             # PSNR calculation using a constant maximum debris velocity of 30
-    #             max_velocity = 30  # Use the known maximum possible velocity
-    #             psnr = 20 * math.log10(max_velocity) - 10 * math.log10(mse_loss.item())
-    #             total_psnr += psnr
-        
-    #     avg_l1_loss = total_l1_loss / len(test_loader)
-    #     avg_mse = total_mse / len(test_loader)
-    #     avg_rmse = math.sqrt(avg_mse)
-    #     avg_psnr = total_psnr / len(test_loader)
-
-    #     print(f'Test L1 Loss: {avg_l1_loss:.4f}')
-    #     print(f'Test MSE: {avg_mse:.4f}')
-    #     print(f'Test RMSE: {avg_rmse:.4f}')
-    #     print(f'Test PSNR: {avg_psnr:.4f}')
 
     def test(self, test_loader):
         self.model.eval()
@@ -203,9 +171,9 @@ class TrainerPairs:
 
         # Scale the data arrays
         min_elevation, max_elevation, min_velocity, max_velocity, min_thickness, max_thickness = self.scaling_factors
-        elevation_scaled = (elevation - min_elevation) / (max_elevation - min_elevation) * 10
-        velocity_scaled = (velocity - min_velocity) / (max_velocity - min_velocity) * 10
-        thickness_scaled = (thickness - min_thickness) / (max_thickness - min_thickness) * 10
+        elevation_scaled = (elevation - min_elevation) / (max_elevation - min_elevation)
+        velocity_scaled = (velocity - min_velocity) / (max_velocity - min_velocity)
+        thickness_scaled = (thickness - min_thickness) / (max_thickness - min_thickness)
 
         # Stack the scaled data arrays to create the input tensor
         input_data = np.stack((elevation_scaled, thickness_scaled, velocity_scaled), axis=0)
@@ -218,20 +186,25 @@ class TrainerPairs:
     def infer(self, initial_input, num_timesteps):
         self.model.eval()
         device = self.device
-        scaling_factors = self.scaling_factors
-        
-        # Ensure initial_input is a PyTorch tensor
-        if not isinstance(initial_input, torch.Tensor):
-            initial_input = torch.tensor(initial_input, dtype=torch.float32)
 
-        # Move the initial input to the same device as the model
-        initial_input = initial_input.to(device)
+        # Ensure initial_input is a PyTorch tensor and move to the device
+        if not isinstance(initial_input, torch.Tensor):
+            initial_input = torch.tensor(initial_input, dtype=torch.float32).to(device)
+        else:
+            initial_input = initial_input.to(device)
 
         # Extract the elevation channel from the initial input
-        elevation = initial_input[0].unsqueeze(0)
+        elevation = initial_input[0].unsqueeze(0)  # This is possibly incorrect if initial_input already includes a batch dimension
 
-        # Initialize the input tensor with the initial input
-        input_tensor = initial_input.unsqueeze(0)  # Add batch dimension
+        print("Elevation shape initial:", elevation.shape)
+
+        # Adjust elevation to have a channels dimension (assuming elevation data is a single channel)
+        elevation = elevation.unsqueeze(1)  # Add a channel dimension making it [1, 1, height, width]
+
+        print("Elevation shape adjusted:", elevation.shape)
+
+        # Initialize the input tensor for the first timestep
+        input_tensor = initial_input.unsqueeze(0)  # Check if a batch dimension is really needed here
 
         # Initialize a dictionary to store the inferred states
         inferred_states = {}
@@ -239,22 +212,24 @@ class TrainerPairs:
         with torch.no_grad():
             for t in range(num_timesteps):
                 # Perform inference
-                next_state = self.model(input_tensor)
+                print("Input tensor shape:", input_tensor.shape)
+                output = self.model(input_tensor)
+                print("Model output shape:", output.shape)
 
-                # Scale and store the output
-                min_elevation, max_elevation, min_velocity, max_velocity, min_thickness, max_thickness = scaling_factors
-                next_state_unscaled = next_state.clone()
-                next_state_unscaled[:, 0, ...] = next_state[:, 0, ...] * (max_thickness - min_thickness) / 10 + min_thickness
-                next_state_unscaled[:, 1, ...] = next_state[:, 1, ...] * (max_velocity - min_velocity) / 10 + min_velocity
-                inferred_states[t + 1] = next_state_unscaled.squeeze(0).cpu().numpy()
+                # Store the inferred state
+                inferred_states[t + 1] = output.squeeze(0).cpu().numpy()
 
-                # Stack the elevation channel with the inferred next state
-                next_state_with_elevation = torch.cat((elevation, next_state.squeeze(0)), dim=0)
+                # Concatenate elevation with output
+                output_with_elevation = torch.cat((elevation, output), dim=1)
+
+                print("Output with elevation shape:", output_with_elevation.shape)
 
                 # Update the input tensor for the next iteration
-                input_tensor = next_state_with_elevation.unsqueeze(0)  # Add batch dimension
+                input_tensor = output_with_elevation
 
         return inferred_states
+
+
 
     def plot_predictions(self, loader, num_predictions=5):
         self.model.eval()
@@ -465,10 +440,12 @@ class TrainerSeries:
         torch.save(self.model.state_dict(), checkpoint_path)
         print(f'Model saved to {checkpoint_path}')
 
-    def load_checkpoint(self, checkpoint_path):
+    def load_checkpoint(self, checkpoint_path, train_loader):
         checkpoint = torch.load(checkpoint_path)
         self.model.load_state_dict(checkpoint)
         print(f'Model loaded from {checkpoint_path}')
+        self.scaling_factors = train_loader.dataset.dataset.scaling_factors
+        self.sequence_length = train_loader.dataset.dataset.sequence_length
 
     def save_losses(self, epoch):
         losses_file = f'losses_epoch_{epoch}.json'
@@ -508,7 +485,6 @@ class TrainerSeries:
         avg_loss = total_loss / len(test_loader)
         print(f'Test Loss: {avg_loss:.4f}')
 
-
     def create_inference_input(self, root_dir, model_number, state_number, array_size):
         # Construct the file paths based on the provided parameters
         model_dir = os.path.join(root_dir, str(model_number))
@@ -523,9 +499,9 @@ class TrainerSeries:
 
         # Scale the data arrays
         min_elevation, max_elevation, min_velocity, max_velocity, min_thickness, max_thickness = self.scaling_factors
-        elevation_scaled = (elevation - min_elevation) / (max_elevation - min_elevation) * 10
-        velocity_scaled = (velocity - min_velocity) / (max_velocity - min_velocity) * 10
-        thickness_scaled = (thickness - min_thickness) / (max_thickness - min_thickness) * 10
+        elevation_scaled = (elevation - min_elevation) / (max_elevation - min_elevation)
+        velocity_scaled = (velocity - min_velocity) / (max_velocity - min_velocity)
+        thickness_scaled = (thickness - min_thickness) / (max_thickness - min_thickness)
 
         # Stack the scaled data arrays to create the input tensor
         input_data = np.stack((elevation_scaled, thickness_scaled, velocity_scaled), axis=0)
@@ -534,7 +510,6 @@ class TrainerSeries:
         input_tensor = torch.from_numpy(input_data).float().to(self.device)
 
         return input_tensor
-
 
     def infer(self, initial_sequence, num_timesteps):
         self.model.eval()
@@ -548,115 +523,143 @@ class TrainerSeries:
 
         # Move the initial sequence to the same device as the model
         initial_sequence = initial_sequence.to(device)
+        print(f"Initial sequence shape: {initial_sequence.shape}")
+        print(f"Initial sequence device: {initial_sequence.device}")
 
         # Extract the elevation channel from the initial sequence
         elevation = initial_sequence[0].unsqueeze(0)
+        print(f"Elevation shape: {elevation.shape}")
 
-        # Initialize the input tensor with the initial sequence
-        input_tensor = [initial_sequence]
+        # Initialize the input tensor with zeros and the initial sequence
+        input_tensor = [torch.zeros_like(initial_sequence) for _ in range(sequence_length - 1)]
+        input_tensor.append(initial_sequence)
+        print(f"Input tensor length: {len(input_tensor)}")
+
 
         # Initialize a dictionary to store the inferred states
         inferred_states = {}
 
         with torch.no_grad():
             for t in range(num_timesteps):
-                # Pad the input tensor with zeros if it's shorter than the sequence length
-                while len(input_tensor) < sequence_length:
-                    input_tensor.insert(0, torch.zeros_like(initial_sequence))
+                print(f"\nTimestep: {t}")
                 
                 # Construct the input sequence
-                input_sequence = torch.stack(input_tensor[-sequence_length:], dim=0)  # Shape: [seq_length, channels, height, width]
+                input_sequence = torch.stack(input_tensor, dim=0)  # Shape: [seq_length, channels, height, width]
                 input_sequence = input_sequence.unsqueeze(0)  # Add batch dimension: [1, seq_length, channels, height, width]
+                print(f"Input sequence shape: {input_sequence.shape}")
 
                 # Perform inference
                 next_state, _ = self.model(input_sequence)
+                print(f"Next state shape: {next_state.shape}")
 
                 # Scale and store the output
                 min_elevation, max_elevation, min_velocity, max_velocity, min_thickness, max_thickness = scaling_factors
                 next_state_unscaled = next_state.clone()
-                next_state_unscaled[:, 0, ...] = next_state[:, 0, ...] * (max_thickness - min_thickness) / 10 + min_thickness
-                next_state_unscaled[:, 1, ...] = next_state[:, 1, ...] * (max_velocity - min_velocity) / 10 + min_velocity
+                next_state_unscaled[:, 0, ...] = next_state[:, 0, ...] * (max_thickness - min_thickness) + min_thickness
+                next_state_unscaled[:, 1, ...] = next_state[:, 1, ...] * (max_velocity - min_velocity) + min_velocity
                 inferred_states[t + 1] = next_state_unscaled.squeeze(0).cpu().numpy()
+                print(f"Inferred state shape at timestep {t + 1}: {inferred_states[t + 1].shape}")
 
                 # Stack the elevation channel with the inferred next state
                 next_state_with_elevation = torch.cat((elevation, next_state.squeeze(0)), dim=0)
+                print(f"Next state with elevation shape: {next_state_with_elevation.shape}")
 
-                # Append the inferred next state with elevation to the input tensor
+                # Remove the oldest state and append the inferred next state with elevation to the input tensor
+                input_tensor.pop(0)
                 input_tensor.append(next_state_with_elevation)
+                print(f"Input tensor length after updating: {len(input_tensor)}")
+
+        print(f"\nFinal inferred states: {list(inferred_states.keys())}")
+
+            # Compare inferred states with each other
+        print("\nComparison of inferred states:")
+        for i in range(1, num_timesteps + 1):
+            for j in range(i + 1, num_timesteps + 1):
+                state_i = inferred_states[i]
+                state_j = inferred_states[j]
+
+                # Calculate mean squared error (MSE) between the states
+                mse = np.mean((state_i - state_j) ** 2)
+
+                # Calculate mean absolute error (MAE) between the states
+                mae = np.mean(np.abs(state_i - state_j))
+
+                print(f"Timestep {i} vs Timestep {j}:")
+                print(f"  Mean Squared Error (MSE): {mse:.4f}")
+                print(f"  Mean Absolute Error (MAE): {mae:.4f}")
 
         return inferred_states
 
 
+    def plot_losses(self):
+        plt.figure(figsize=(10, 5))
+        plt.plot(self.training_losses, label='Training Loss')
+        plt.plot(self.validation_losses, label='Validation Loss')
+        plt.title('Training and Validation Losses')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.grid(True)
+        plt.tight_layout()
 
-    # def plot_losses(self):
-    #     plt.figure(figsize=(10, 5))
-    #     plt.plot(self.training_losses, label='Training Loss')
-    #     plt.plot(self.validation_losses, label='Validation Loss')
-    #     plt.title('Training and Validation Losses')
-    #     plt.xlabel('Epochs')
-    #     plt.ylabel('Loss')
-    #     plt.legend()
-    #     plt.grid(True)
-    #     plt.tight_layout()
+        # Save the plot as an image file
+        plot_file = 'loss_plot.png'
+        plot_path = os.path.join(self.checkpoint_dir, plot_file)
+        plt.savefig(plot_path)
+        print(f'Loss plot saved to {plot_path}')
 
-    #     # Save the plot as an image file
-    #     plot_file = 'loss_plot.png'
-    #     plot_path = os.path.join(self.checkpoint_dir, plot_file)
-    #     plt.savefig(plot_path)
-    #     print(f'Loss plot saved to {plot_path}')
+        # Show the plot
+        plt.show()
 
-    #     # Show the plot
-    #     plt.show()
+    def plot_predictions(self, test_loader, num_predictions=5):
+        self.model.eval()
+        with torch.no_grad():
+            for i, (sequence, next_state) in enumerate(test_loader):
+                if i >= num_predictions:
+                    break
 
-    # def plot_predictions(self, test_loader, num_predictions=5):
-    #     self.model.eval()
-    #     with torch.no_grad():
-    #         for i, (sequence, next_state) in enumerate(test_loader):
-    #             if i >= num_predictions:
-    #                 break
+                sequence = sequence.to(self.device)
+                next_state = next_state.to(self.device)
 
-    #             sequence = sequence.to(self.device)
-    #             next_state = next_state.to(self.device)
+                predictions, _ = self.model(sequence)
 
-    #             predictions, _ = self.model(sequence)
+                # Move the data back to the CPU for plotting
+                next_state = next_state.cpu().numpy()
+                predictions = predictions.cpu().numpy()
 
-    #             # Move the data back to the CPU for plotting
-    #             next_state = next_state.cpu().numpy()
-    #             predictions = predictions.cpu().numpy()
+                # Plot the expected and predicted results side by side
+                fig, axes = plt.subplots(3, 2, figsize=(12, 18))
 
-    #             # Plot the expected and predicted results side by side
-    #             fig, axes = plt.subplots(3, 2, figsize=(12, 18))
+                # Process and plot each type of data (thickness, velocity)
+                for index, title in enumerate(['Thickness', 'Velocity']):
+                    # Determine color limits based on the expected values
+                    vmin = np.min(next_state[0, index])
+                    vmax = np.max(next_state[0, index])
 
-    #             # Process and plot each type of data (thickness, velocity)
-    #             for index, title in enumerate(['Thickness', 'Velocity']):
-    #                 # Determine color limits based on the expected values
-    #                 vmin = np.min(next_state[0, index])
-    #                 vmax = np.max(next_state[0, index])
+                    # Expected
+                    im = axes[index, 0].imshow(next_state[0, index], cmap='viridis', vmin=vmin, vmax=vmax)
+                    axes[index, 0].set_title(f"Expected {title}")
+                    axes[index, 0].axis('off')
+                    fig.colorbar(im, ax=axes[index, 0], orientation='vertical')
 
-    #                 # Expected
-    #                 im = axes[index, 0].imshow(next_state[0, index], cmap='viridis', vmin=vmin, vmax=vmax)
-    #                 axes[index, 0].set_title(f"Expected {title}")
-    #                 axes[index, 0].axis('off')
-    #                 fig.colorbar(im, ax=axes[index, 0], orientation='vertical')
+                    # Predicted
+                    im = axes[index, 1].imshow(predictions[0, index], cmap='viridis', vmin=vmin, vmax=vmax)
+                    axes[index, 1].set_title(f"Predicted {title}")
+                    axes[index, 1].axis('off')
+                    fig.colorbar(im, ax=axes[index, 1], orientation='vertical')
 
-    #                 # Predicted
-    #                 im = axes[index, 1].imshow(predictions[0, index], cmap='viridis', vmin=vmin, vmax=vmax)
-    #                 axes[index, 1].set_title(f"Predicted {title}")
-    #                 axes[index, 1].axis('off')
-    #                 fig.colorbar(im, ax=axes[index, 1], orientation='vertical')
+                # Compute and plot the differences for thickness and velocity
+                for index, title in enumerate(['Thickness', 'Velocity']):
+                    diff = np.abs(next_state[0, index] - predictions[0, index])
+                    vmax = np.max(diff)  # Dynamic range based on the actual differences observed
 
-    #             # Compute and plot the differences for thickness and velocity
-    #             for index, title in enumerate(['Thickness', 'Velocity']):
-    #                 diff = np.abs(next_state[0, index] - predictions[0, index])
-    #                 vmax = np.max(diff)  # Dynamic range based on the actual differences observed
+                    im = axes[2, index].imshow(diff, cmap='hot', vmin=0, vmax=vmax)
+                    axes[2, index].set_title(f"Difference in {title}")
+                    axes[2, index].axis('off')
+                    fig.colorbar(im, ax=axes[2, index], orientation='vertical')
 
-    #                 im = axes[2, index].imshow(diff, cmap='hot', vmin=0, vmax=vmax)
-    #                 axes[2, index].set_title(f"Difference in {title}")
-    #                 axes[2, index].axis('off')
-    #                 fig.colorbar(im, ax=axes[2, index], orientation='vertical')
-
-    #             plt.tight_layout()
-    #             plt.show()
+                plt.tight_layout()
+                plt.show()
 
 class CustomDebrisLoss(nn.Module):
     def __init__(self, loss_fn_zero=nn.MSELoss(), loss_fn_debris=nn.L1Loss(), debris_weight=0.75):

LandSlideDyna/pre/final_metadata.py
@@ -52,7 +52,6 @@ class FinallMetadata:
         self._load_state_and_summary_data(self.velocity_dir, 'velocity')
 
 
-
     def _load_state_and_summary_data(self, directory, data_type):
         """Loads state-specific data and summary data from the given directory.
         
@@ -85,90 +84,6 @@ class FinallMetadata:
     def calculate_metadata(self):
         self.calculate_bounding_boxes()
     
-    # def calculate_metadata(self):
-    #     """Calculate and update the metadata based on the provided data."""
-    #     # Assuming that elevation_array_dict contains 'x_values', 'y_values', 'z_values'
-    #     elevation_dict = self.data['elevation_array_dict']
-    #     self.metadata['min_x_value'] = np.min(elevation_dict['x_values'])
-    #     self.metadata['max_x_value'] = np.max(elevation_dict['x_values'])
-    #     self.metadata['min_y_value'] = np.min(elevation_dict['y_values'])
-    #     self.metadata['max_y_value'] = np.max(elevation_dict['y_values'])
-    #     self.metadata['min_z_value'] = np.min(elevation_dict['z_values'])
-    #     self.metadata['max_z_value'] = np.max(elevation_dict['z_values'])
-
-    #     # Dimensions based on min and max values
-    #     self.metadata['x_dimension'] = self.metadata['max_x_value'] - self.metadata['min_x_value']
-    #     self.metadata['y_dimension'] = self.metadata['max_y_value'] - self.metadata['min_y_value']
-    #     self.metadata['z_dimension'] = self.metadata['max_z_value'] - self.metadata['min_z_value']
-
-    #     # Grid resolution calculation
-    #     self.metadata['grid_resolution_x'] = self.calculate_grid_resolution(elevation_dict['x_values'])
-    #     self.metadata['grid_resolution_y'] = self.calculate_grid_resolution(elevation_dict['y_values'])
-
-    #     # Total number of states and average timestep calculation
-    #     state_timesteps = self.data['state_timesteps']
-    #     self.metadata['total_number_of_states'] = len(state_timesteps)
-    #     self.metadata['average_timestep'] = self.calculate_average_timestep()
-    #     self.metadata['timesteps'] = state_timesteps
-
-    #     # Calculate more metadata
-    #     self.check_empty_states()
-
-    #     self.calculate_bounding_boxes()
-
-    #     self.calculate_state_statistics()
-
-    #     self.calculate_debris_metrics()
-
-    # def calculate_grid_resolution(self, values):
-    #     """Calculate the grid resolution based on the axis values.
-
-    #     Args:
-    #         values (np.ndarray): The values along an axis.
-
-    #     Returns:
-    #         float: The calculated grid resolution.
-    #     """
-    #     return np.diff(values).mean() if len(values) > 1 else None
-
-    # # Placeholder for calculate_average_timestep method
-    # def calculate_average_timestep(self):
-    #     """Calculate the average timestep from a sequence of timestamps stored in self.data.
-
-    #     Returns:
-    #         float or None: The average timestep calculated as the mean of the difference between sequential timestamps,
-    #                     or None if insufficient timestamps.
-    #     """
-    #     timestamps = pd.Series(self.data['state_timesteps'].values())
-    #     sorted_timestamps = timestamps.sort_values().values
-    #     if len(sorted_timestamps) > 1:
-    #         timestep_differences = np.diff(sorted_timestamps)
-    #         return np.mean(timestep_differences)
-    #     return None
-
-
-
-    # def check_empty_states(self):
-    #     """Checks each state and records which ones are empty (all zeroes).
-        
-    #     The result is stored in the class attribute `metadata` with a key 'empty_states', mapping each state number
-    #     to a boolean indicating whether it is empty or not.
-    #     """
-    #     # Initialize empty states metadata
-    #     self.metadata['empty_states'] = {}
-
-    #     # Get all state numbers from the thickness and velocity dicts (assuming they have the same keys)
-    #     state_numbers = self.data['thickness_state_array_dict'].keys()
-
-    #     # Loop over all state numbers and check for empty states
-    #     for state_number in state_numbers:
-    #         thickness_values = self.data['thickness_state_array_dict'][state_number]
-    #         velocity_values = self.data['velocity_state_array_dict'][state_number]
-
-    #         # Check if all values for a state are zero for both thickness and velocity
-    #         is_empty = np.all(thickness_values == 0) and np.all(velocity_values == 0)
-    #         self.metadata['empty_states'][state_number] = is_empty
-
 
     def calculate_bounding_boxes(self):
         """Calculates and stores the bounding box and its dimensions of the debris for every state,
@@ -228,88 +143,6 @@ class FinallMetadata:
         self.metadata['overall_bounding_box']['center_x'] = (global_min_x + global_max_x) / 2
         self.metadata['overall_bounding_box']['center_y'] = (global_min_y + global_max_y) / 2
 
-    # def calculate_state_statistics(self):
-    #     """Calculates and stores the max and average values of thickness and velocity for each state."""
-    #     # Initialize the statistics metadata
-    #     self.metadata['state_statistics'] = {}
-
-    #     # Use the keys from thickness_state_array_dict as they should match the velocity_state_array_dict keys
-    #     state_numbers = self.data['thickness_state_array_dict'].keys()
-
-    #     # Iterate over states
-    #     for state_number in state_numbers:
-    #         thickness_values = self.data['thickness_state_array_dict'][state_number]
-    #         velocity_values = self.data['velocity_state_array_dict'][state_number]
-
-    #         # Calculate max values
-    #         max_thickness = np.max(thickness_values)
-    #         max_velocity = np.max(velocity_values)
-
-    #         # Calculate averages (considering zeroes)
-    #         avg_thickness_incl_zeroes = np.mean(thickness_values)
-    #         avg_velocity_incl_zeroes = np.mean(velocity_values)
-
-    #         # Calculate averages (ignoring zeroes)
-    #         avg_thickness_excl_zeroes = np.mean(thickness_values[thickness_values > 0]) if np.any(thickness_values > 0) else 0
-    #         avg_velocity_excl_zeroes = np.mean(velocity_values[velocity_values > 0]) if np.any(velocity_values > 0) else 0
-
-    #         # Store in metadata
-    #         self.metadata['state_statistics'][state_number] = {
-    #             'max_thickness': max_thickness,
-    #             'max_velocity': max_velocity,
-    #             'avg_thickness_incl_zeroes': avg_thickness_incl_zeroes,
-    #             'avg_velocity_incl_zeroes': avg_velocity_incl_zeroes,
-    #             'avg_thickness_excl_zeroes': avg_thickness_excl_zeroes,
-    #             'avg_velocity_excl_zeroes': avg_velocity_excl_zeroes
-    #     }
-
-
-    # def calculate_debris_metrics(self):
-    #     """Calculates and stores various metrics for each state that could help identify a stopping condition."""
-    #     # Initialize the metrics metadata
-    #     self.metadata['debris_metrics'] = {}
-
-    #     previous_occupied_area = None
-
-    #     # Use the keys from thickness_state_array_dict as they should match the velocity_state_array_dict keys
-    #     state_numbers = self.data['thickness_state_array_dict'].keys()
-
-    #     # Iterate over states
-    #     for state_number in state_numbers:
-    #         thickness_values = self.data['thickness_state_array_dict'][state_number]
-    #         velocity_values = self.data['velocity_state_array_dict'][state_number]
-
-    #         # Calculate Occupied Area Change
-    #         occupied_area = np.count_nonzero(thickness_values)
-    #         occupied_area_change = self._calculate_occupied_area_change(previous_occupied_area, occupied_area)
-    #         previous_occupied_area = occupied_area  # Update for next iteration
-
-    #         # Calculate Standard Deviation of Velocity
-    #         std_dev_velocity = np.std(velocity_values)
-
-    #         # Calculate Standard Deviation of Thickness
-    #         std_dev_thickness = np.std(thickness_values)
-
-    #         # Calculate Temporal Stability
-    #         # Assuming temporal stability is represented by the variability in the occupied area across frames.
-    #         # This could be refined based on a more sophisticated definition of temporal stability.
-    #         temporal_stability = 0 if state_number == min(state_numbers) else self.metadata['debris_metrics'][prev_state_number]['occupied_area_change']
-
-    #         # Store in metadata
-    #         self.metadata['debris_metrics'][state_number] = {
-    #             'occupied_area_change': occupied_area_change,
-    #             'std_dev_velocity': std_dev_velocity,
-    #             'std_dev_thickness': std_dev_thickness,
-    #             'temporal_stability': temporal_stability
-    #         }
-    #         prev_state_number = state_number  # Store the current state number for the next iteration
-
-    # def _calculate_occupied_area_change(self, previous_area, current_area):
-    #     """Calculate the change in occupied area from the previous to the current state."""
-    #     if previous_area is None:  # Handle the case where this is the first state
-    #         return 0
-    #     return current_area - previous_area
-
     
     def save_metadata(self):
         """Saves the metadata to a JSON file named with the model_id in the model_dir directory."""
@@ -327,6 +160,7 @@ class FinallMetadata:
         # Write the metadata to the file
         with open(filepath, 'w') as f:
             json.dump(metadata_serializable, f, indent=4)  # use indent for pretty-printing
+
 
     def _convert_to_serializable(self, data):
         """Recursively convert NumPy types to Python types for JSON serialization."""

LandSlideDyna/pre/intial_processing.py
@@ -423,6 +423,7 @@ class ArrayConverter:
         }
         return state_dict
 
+
     @staticmethod
     def dataframe_to_elevation_array(df, x_col='Shell_X', y_col='Shell_Y', z_col='Shell_Z'):
         """
@@ -458,7 +459,6 @@ class ArrayConverter:
             elevation_array[y_idx, x_idx] = row[z_col]
 
         return elevation_array, unique_x, unique_y
-
 
 
     @staticmethod
@@ -503,7 +503,8 @@ class ArrayConverter:
                 state_arrays[i][y_idx, x_idx] = row[state_col]
 
         return state_arrays, state_labels, unique_x, unique_y
-    
+
+
     @staticmethod
     def flatten_to_max_value(state_arrays):
         """Flatten a list of 2D state arrays into a single array, keeping the maximum value.
@@ -517,10 +518,6 @@ class ArrayConverter:
         # Stack the arrays along a new dimension and then take the max along this new dimension
         max_value_array = np.max(np.stack(state_arrays), axis=0)
         return max_value_array
-
-
-
-    
     
 
 class InitialProcessPipeline:
@@ -550,6 +547,7 @@ class InitialProcessPipeline:
         """
         return self.data.get(key)
 
+
     def _export_array(self, array, subdir, filename):
         """Helper function to export a NumPy array to a file.
 
@@ -564,6 +562,7 @@ class InitialProcessPipeline:
 
         file_path = os.path.join(dir_path, filename)
         np.save(file_path, array)
+
 
     def export_processed_data(self):
         """Export processed data to the specified directory structure."""

LandSlideDyna/pre/metadata.py
@@ -87,6 +87,7 @@ class ModelMetadata:
         self.data[f'{data_type}_state_array_dict'] = {str(k): v for k, v in sorted_state_data_dict.items()}
         self.data[f'{data_type}_summary_data_dict'] = summary_data_dict
 
+
     def _load_state_timesteps(self):
         """Loads the state timesteps from a CSV file and adds them to the data dictionary."""
         csv_file_path = os.path.join(self.states_dir, f"{self.model_id}_states.csv")
@@ -141,6 +142,7 @@ class ModelMetadata:
 
         self.calculate_debris_metrics()
 
+
     def calculate_grid_resolution(self, values):
         """Calculate the grid resolution based on the axis values.
 
@@ -152,7 +154,8 @@ class ModelMetadata:
         """
         return np.diff(values).mean() if len(values) > 1 else None
 
-    # Placeholder for calculate_average_timestep method
+
+
     def calculate_average_timestep(self):
         """Calculate the average timestep from a sequence of timestamps stored in self.data.
 
@@ -166,7 +169,6 @@ class ModelMetadata:
             timestep_differences = np.diff(sorted_timestamps)
             return np.mean(timestep_differences)
         return None
-
 
 
     def check_empty_states(self):
@@ -249,6 +251,7 @@ class ModelMetadata:
         self.metadata['overall_bounding_box']['center_x'] = (global_min_x + global_max_x) / 2
         self.metadata['overall_bounding_box']['center_y'] = (global_min_y + global_max_y) / 2
 
+
     def calculate_state_statistics(self):
         """Calculates and stores the max and average values of thickness and velocity for each state."""
         # Initialize the statistics metadata
@@ -325,6 +328,7 @@ class ModelMetadata:
             }
             prev_state_number = state_number  # Store the current state number for the next iteration
 
+
     def _calculate_occupied_area_change(self, previous_area, current_area):
         """Calculate the change in occupied area from the previous to the current state."""
         if previous_area is None:  # Handle the case where this is the first state
@@ -348,6 +352,7 @@ class ModelMetadata:
         # Write the metadata to the file
         with open(filepath, 'w') as f:
             json.dump(metadata_serializable, f, indent=4)  # use indent for pretty-printing
+
 
     def _convert_to_serializable(self, data):
         """Recursively convert NumPy types to Python types for JSON serialization."""